9aa4d35757a6609aa3a428ca169aae0332071aba
Stephanie Cristina Sabán Cárcamo <201800233@colsanjose.edu.gt>
2021-08-09 13:51:45 -0600

    listooo.

---
+++ App 1 Stephanie/App 1 Stephanie/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace App_1_Stephanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string R = "s";
+            while (R == "s")
+            {
+                Console.WriteLine("Escribe el nombre del archivo: ");
+                string archivo = Console.ReadLine();
+
+
+                StreamWriter nombre;
+                nombre = File.CreateText(archivo + ".txt");
+                nombre.WriteLine("HOLA AQUI SE CREO EL ARCHIVO CON EL NOMBRE QUE ELIGIÓ...");
+                nombre.Close();
+
+
+                Console.WriteLine("Quieres crear otro archivo?");
+                Console.WriteLine("s/n");
+                R = Console.ReadLine();
+                Console.Clear();
+                if (R == "no")
+                {
+                    return;
+                }
+            }
+    }    }
+}
